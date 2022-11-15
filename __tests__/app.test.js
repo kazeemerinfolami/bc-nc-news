e87eb40c6693c5637ba5ae9f74b19d7b98b07af5
“kazeemerinfolami” <erinfolamibolaji2010@gmail.com>
2022-11-15 12:10:30 +0000

    change result to topics on the controller.js ||  create a separate describe block for the URL not found on the app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -23,19 +23,19 @@ afterAll(() => {
   return db.end();
 });
 
-describe("/api/topics", () => {
+describe("GET /api/topics", () => {
   test("GET 200 - respond with an object containing all the slug", () => {
     return request(app)
       .get("/api/topics")
       .expect(200)
       .then((res) => {
-        const { result } = res.body;
-        //if result is an array
-        expect(result).toBeInstanceOf(Array);
+        const { topics } = res.body;
+        //if topic is an array
+        expect(topics).toBeInstanceOf(Array);
         //length to be 3
-        expect(result.length).toBe(3);
+        expect(topics.length).toBe(3);
         //for each slug object should match the test object
-        result.forEach((slugTopic) => {
+        topics.forEach((slugTopic) => {
           expect(slugTopic).toMatchObject({
             slug: expect.any(String),
             description: expect.any(String),
@@ -43,7 +43,10 @@ describe("/api/topics", () => {
         });
       });
   });
-  test("GET 404 - if not found", () => {
+});
+
+describe("GET /api/topics/gysyugysyugisuygyugsu", () => {
+  test("GET 404 - if URL not found", () => {
     return request(app)
       .get("/api/topics/gysyugysyugisuygyugsu")
       .expect(404)
--- controller/nc_news.controller.js
+++ controller/nc_news.controller.js
@@ -2,8 +2,8 @@ const { fetchTopics } = require("../model/nc_news.model");
 
 exports.getTopics = (req, res, next) => {
   fetchTopics()
-    .then((result) => {
-      res.status(200).send({ result });
+    .then((topics) => {
+      res.status(200).send({ topics });
     })
     .catch((err) => {
       next(err);
